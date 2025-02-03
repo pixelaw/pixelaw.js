31a0fdc9aecaf1eba5c471ff058f46cbb7ba961b
thiscaspar <thiscaspar@gmail.com>
2025-02-03 10:26:03 +0800

    wip

--- packages/core-dojo/dist/DojoEngine.d.ts
+++
@@ -1,7 +0,0 @@
-import '@pixelaw/core';
-export { DojoEngine } from './index.js';
-import 'starknet';
-import '@cartridge/connector/controller';
-import '@dojoengine/core';
-import '@dojoengine/create-burner';
-import '@dojoengine/sdk';
