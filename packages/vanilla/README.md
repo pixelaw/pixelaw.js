3cf39360d1d9f5f336271c87553208d141f039ee
thiscaspar <thiscaspar@gmail.com>
2025-02-10 08:52:19 +0800

    moved vanilla to dedicated repo

--- packages/vanilla/README.md
+++
@@ -1,1 +0,0 @@
-# PixelAW.JS
--- packages/vanilla/src/declarations.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "*.graphql" {
-    import type { DocumentNode } from "graphql"
-    const value: DocumentNode
-    export = value
-}
-
-declare module "*.module.css";
