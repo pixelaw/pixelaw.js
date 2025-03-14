0cde3a5ef5637ad26c8df989ab27f259ad383f14
thiscaspar <thiscaspar@gmail.com>
2025-03-14 09:31:59 +0800

    devnet is identical to sepolia, removing

--- packages/react-dojo/src/hooks/StarknetChainProvider.tsx
+++ packages/react-dojo/src/hooks/StarknetChainProvider.tsx
@@ -1,11 +1,11 @@
 import type {ChainProviderProps} from "@pixelaw/react"
-import {devnet, mainnet, sepolia} from "@starknet-react/chains"
+import { mainnet, sepolia} from "@starknet-react/chains"
 import {publicProvider, StarknetConfig} from "@starknet-react/core"
 import {ConnectorProvider} from "./ConnectorProvider"
 
 export const StarknetChainProvider: React.FC<ChainProviderProps> = ({ children }) => {
     return (
-        <StarknetConfig chains={[mainnet, devnet, sepolia]} provider={publicProvider()} connectors={[]}>
+        <StarknetConfig chains={[mainnet, sepolia]} provider={publicProvider()} connectors={[]}>
             <ConnectorProvider>{children}</ConnectorProvider>
         </StarknetConfig>
     )
